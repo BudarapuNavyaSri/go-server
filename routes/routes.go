12b8083a8aded8acbd606c9b69ac4d2b59ff988e
Navya Sri <budarapunavya_sri@epam.com>
2025-02-10 02:08:03 +0530

    routes

--- routes/routes.go
+++ routes/routes.go
@@ -7,17 +7,16 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
 func InitializeRoutes() {
-	router:= mux.NewRouter()
+	router := mux.NewRouter()
 
 	router.HandleFunc("/todos", handlers.GetTodos).Methods("GET")
 	router.HandleFunc("/todos/add", handlers.AddTodo).Methods("POST")
 	router.HandleFunc("/todos/{id}/update", handlers.UpdateTodo).Methods("PUT")
 	router.HandleFunc("/todos/{id}/delete", handlers.DeleteTodo).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8080",router))
-
-	
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 	// http.HandleFunc("/todos", handlers.GetTodos)
 	// http.HandleFunc("/todos/add", handlers.AddTodo)
